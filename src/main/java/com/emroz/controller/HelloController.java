3a8e3125c01e5e1c47a9afd762502281f8b9f02e
Emroz.Habib <Emroz.Habib@target.com>
2015-03-05 09:48:04 -0600

    FIrst Commit

---
+++ src/main/java/com/emroz/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.emroz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value="/greeting")
+	public String sayHello(Model model){
+		
+		model.addAttribute("greeting", "Hello Java Spring");
+		return "hello";
+		
+	}
+	
+	@RequestMapping(value="/index")
+	public String index(Model model){
+		
+		return "forward:index.jsp";
+		
+	}
+	
+
+}
---
+++ src/main/java/com/emroz/view/PhoneConstraintValidator.java
@@ -0,0 +1,21 @@
+package com.emroz.view;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PhoneConstraintValidator implements ConstraintValidator<Phone, String>{
+
+	@Override
+	public void initialize(Phone arg0) {
+	}
+
+	@Override
+	public boolean isValid(String phoneField, ConstraintValidatorContext ctx) {
+		if(phoneField == null) {
+			return false;
+		}
+		
+		return phoneField.matches("[0-9()-]*");
+	}
+
+}
